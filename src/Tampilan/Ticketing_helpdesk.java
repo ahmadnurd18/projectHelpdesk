a9e3dbec3f6aedbbd657ebca4f6c103a3619d53b
ANGGI_SATRIAWAN <ANGGI_SATRIAWAN@MTT-L38>
2025-04-29 20:43:05 +0700

    Tambah Tampilan login,dashbord dan user form

---
+++ src/Tampilan/Ticketing_helpdesk.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Tampilan;
+
+/**
+ *
+ * @author ANGGI_SATRIAWAN
+ */
+public class Ticketing_helpdesk {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
